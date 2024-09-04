d43b70160424c9df963058a741edc6b8bbb9d64e
hungnguyenrfh02 <177386014+hungnguyenrfh@users.noreply.github.com>
2024-09-04 22:54:28 +0700

    Add a ProductUpdateRequest class

--- dream-shops/src/main/java/com/hung/dreamshops/request/ProductUpdateRequest.java
+++ dream-shops/src/main/java/com/hung/dreamshops/request/ProductUpdateRequest.java
@@ -1,4 +1,17 @@
 package com.hung.dreamshops.request;
 
+import com.hung.dreamshops.model.Category;
+import lombok.Data;
+
+import java.math.BigDecimal;
+
+@Data
 public class ProductUpdateRequest {
+    private Long id;
+    private String name;
+    private String description;
+    private BigDecimal price;
+    private int inventory;
+    private String brand;
+    private Category category;
 }
