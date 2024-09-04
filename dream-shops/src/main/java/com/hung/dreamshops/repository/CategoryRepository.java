8e787551b693825badea815d5dddaec206aeb632
hungnguyenrfh02 <177386014+hungnguyenrfh@users.noreply.github.com>
2024-09-04 22:53:17 +0700

    Complete the addProduct method in the ProductService class

---
+++ dream-shops/src/main/java/com/hung/dreamshops/repository/CategoryRepository.java
@@ -0,0 +1,8 @@
+package com.hung.dreamshops.repository;
+
+import com.hung.dreamshops.model.Category;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CategoryRepository extends JpaRepository<Category, Long> {
+  Category findByName(String name);
+}
--- dream-shops/src/main/java/com/hung/dreamshops/service/product/ProductService.java
+++ dream-shops/src/main/java/com/hung/dreamshops/service/product/ProductService.java
@@ -3,6 +3,7 @@ package com.hung.dreamshops.service.product;
 import com.hung.dreamshops.exceptions.ProductNotFoundException;
 import com.hung.dreamshops.model.Category;
 import com.hung.dreamshops.model.Product;
+import com.hung.dreamshops.repository.CategoryRepository;
 import com.hung.dreamshops.repository.ProductRepository;
 import com.hung.dreamshops.request.AddProductRequest;
 import lombok.AllArgsConstructor;
@@ -10,17 +11,28 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
 public class ProductService implements IProductService {
     private final ProductRepository productRepository;
+    private final CategoryRepository categoryRepository;
 
     @Override
-    public Product addProduct(AddProductRequest product) {
+    public Product addProduct(AddProductRequest request) {
         //check if the category is found in the DB
         //If Yes, set it as the new product category
-        return null;
+        //If No, then save it as a new category
+        //then set it as the new product category
+
+        Category category = Optional.ofNullable(categoryRepository.findByName(request.getCategory().getName()))
+                .orElseGet(() -> {
+                    Category newCategory = new Category(request.getCategory().getName());
+                    return categoryRepository.save(newCategory);
+                });
+        request.setCategory(category);
+        return productRepository.save(createProduct(request,category));
     }
 
     //private method to help add product
