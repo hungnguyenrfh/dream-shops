34938427e83a57dfe2699bc618e598e96b5f0869
hungnguyenrfh02 <177386014+hungnguyenrfh@users.noreply.github.com>
2024-09-05 11:27:48 +0700

    Complete the CategoryService class

--- dream-shops/src/main/java/com/hung/dreamshops/exceptions/ProductNotFoundException.java
+++ dream-shops/src/main/java/com/hung/dreamshops/exceptions/ProductNotFoundException.java
@@ -1,9 +1,6 @@
 package com.hung.dreamshops.exceptions;
 
 public class ProductNotFoundException extends RuntimeException{
-    public ProductNotFoundException(){
-        super();
-    }
     public ProductNotFoundException(String message){
         super(message);
     }
