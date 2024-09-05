9cb355f092eb40762ed1aa293471367a6684b0bf
hungnguyenrfh02 <177386014+hungnguyenrfh@users.noreply.github.com>
2024-09-05 11:28:17 +0700

    Complete the CategoryService class

--- dream-shops/src/main/java/com/hung/dreamshops/exceptions/ResourceNotFoundException.java
+++ dream-shops/src/main/java/com/hung/dreamshops/exceptions/ResourceNotFoundException.java
@@ -1,11 +1,7 @@
 package com.hung.dreamshops.exceptions;
 
 public class ResourceNotFoundException extends RuntimeException{
-    public ResourceNotFoundException(String message){
+    public ResourceNotFoundException(String message) {
         super(message);
-    }
-
-    public ResourceNotFoundException(){
-        super();
     }
 }
